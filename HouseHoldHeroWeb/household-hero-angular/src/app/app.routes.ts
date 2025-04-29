7383115424087513c64eb4125290bf37248619f6
Khader Jeries <khader.jeryes@gmail.com>
2025-04-29 11:16:31 +0300

    fix footer links

--- HouseHoldHeroWeb/household-hero-angular/src/app/app.routes.ts
+++ HouseHoldHeroWeb/household-hero-angular/src/app/app.routes.ts
@@ -4,9 +4,12 @@ import { GuestHomeContentComponent } from './guest/home-content/guest-home-conte
 import { LoginComponent } from './guest/login/login.component';
 import { RegistrationComponent } from './guest/registration/registration.component';
 import { OtpVerificationComponent } from './guest/otp-verification/otp-verification.component';
-import { UserComponent } from './user/user.component';
 import { ForgotPasswordComponent } from './guest/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './guest/reset-password/reset-password.component';
+import { UserComponent } from './user/user.component';
+import { ContactComponent } from './guest/contact/contact.component';
+import { PrivacyPolicyComponent } from './guest/privacy-policy/privacy-policy.component';
+import { TermsServiceComponent } from './guest/terms-of-service/terms-of-service.component';
 
 export const routes: Routes = [
   { 
@@ -19,7 +22,10 @@ export const routes: Routes = [
       { path: 'guest/registration', component: RegistrationComponent },
       { path: 'guest/otp-verification', component: OtpVerificationComponent },
       { path: 'guest/forgot-password', component: ForgotPasswordComponent },
-      { path: 'guest/reset-password', component: ResetPasswordComponent }
+      { path: 'guest/reset-password', component: ResetPasswordComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'privacy-policy', component: PrivacyPolicyComponent },
+      { path: 'terms-of-service', component: TermsServiceComponent }
     ]
   },
   { 
--- HouseHoldHeroWeb/household-hero-angular/src/app/guest/terms-of-service/terms-of-service.component.ts
+++ HouseHoldHeroWeb/household-hero-angular/src/app/guest/terms-of-service/terms-of-service.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-terms-service',
   standalone: true,
-  templateUrl: './terms-service.component.html',
-  styleUrl: './terms-service.component.css'
+  templateUrl: './terms-of-service.component.html',
+  styleUrl: './terms-of-service.component.css'
 })
 export class TermsServiceComponent {
   lastUpdated: string = 'April 29, 2025';
