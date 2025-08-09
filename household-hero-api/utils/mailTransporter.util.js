1635eb16fb4c9822e2cded7360ba17e0a55ec23e
Khader1jeries <97103522+Khader1jeries@users.noreply.github.com>
2025-08-09 12:27:12 +0300

    .

--- household-hero-api/utils/mailTransporter.util.js
+++ household-hero-api/utils/mailTransporter.util.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "khader.jeryes@gmail.com", // e.g., 'your_email@gmail.com'
-    pass: "miaupwzatqppzsom", // use Gmail App Password
+    pass: "fxmwmjfozudmtdjt", // use Gmail App Password
   },
 });
 
