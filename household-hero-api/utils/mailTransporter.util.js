718ab86d77aaf2a776d0e73c0a85742a1add972b
Khader1jeries <khader.jeryes@gmail.com>
2025-09-13 17:45:19 +0300

    .

--- household-hero-api/utils/mailTransporter.util.js
+++ household-hero-api/utils/mailTransporter.util.js
@@ -5,11 +5,8 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "khader.jeryes@gmail.com", // e.g., 'your_email@gmail.com'
-<<<<<<< HEAD
-    pass: "jbhdbxzcjnrtrijf", // use Gmail App Password
-=======
+
     pass: "fxmwmjfozudmtdjt", // use Gmail App Password
->>>>>>> 25404cf2de5ec447f0f0129fc36c328f7eaf52e6
   },
 });
 
